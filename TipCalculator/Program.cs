3250f30c0a07484368a9675677b6019360fd6671
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-02-27 20:06:53 -0600

    added method

--- TipCalculator/Program.cs
+++ TipCalculator/Program.cs
@@ -8,7 +8,6 @@ namespace TipCalculator
         {
             decimal bill = 0;
             decimal tipPercent = 0;
-
             //Here is how you might do it if you only wanted to give them a limited amount of retries
             int retries = 0;
             int maxRetries = 5;
@@ -25,14 +24,7 @@ namespace TipCalculator
                 }
 
                 retries++;
-                if(retries < maxRetries)
-                {
-                    Console.WriteLine($"The value {billInput} is not a valid bill amount. Please try again.");            
-                }
-                else
-                {
-                    Console.WriteLine("You have reached the max number of retries.");
-                }
+                PrintRetryResponse(retries, maxRetries, billInput);  
             }
 
             retries = 0;
@@ -48,16 +40,9 @@ namespace TipCalculator
                 }
 
                 retries++;
-                if (retries < maxRetries)
-                {
-                    Console.WriteLine($"The value {tipPercentInput} is not a valid tip. Please try again.");
-                }
-                else
-                {
-                    Console.WriteLine("You have reached the max number of retries.");
-                }
+                PrintRetryResponse(retries, maxRetries, tipPercentInput);
             }
-
+           
             decimal tipAmount = Math.Round(tipPercent * bill / 100, 2);
             decimal totalBill = Math.Round(bill + tipAmount, 2);
 
@@ -66,5 +51,23 @@ namespace TipCalculator
 
             Console.ReadLine();
         }
+
+        static void PrintRetryResponse(int retries, int maxRetries, string userInput)
+        {
+            if (retries < maxRetries)
+            {
+                Console.WriteLine($"The value {userInput} is not a valid amount. Please try again.");
+            }
+            else
+            {
+                Console.WriteLine("You have reached the max number of retries.");
+            }
+        }
+            /*
+             What we need for this method: retries, maxRetries, billInput
+             */
+             
+
+
     }
 }
